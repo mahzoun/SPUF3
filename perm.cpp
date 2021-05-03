fb249c91d99fb1b016576debd662393dd344d7b5
mahzoun <mohammad.mahzoun@gmail.com>
2021-05-03 16:17:48 +0200

    add r out of n permutation

--- perm.cpp
+++ perm.cpp
@@ -1,44 +1,61 @@
 #include <algorithm>
 #include <iostream>
+#include <vector>
 #include <string> 
 #include <map>
 
-const int N = 10, tenfact = 3628800;
+
+const int N = 5, R = 3;
 
 using namespace std;
 
 int main(){
+
 	ios_base::sync_with_stdio(false);
 	map<string, uint64_t> m;
 	map<string, uint64_t>::iterator it;
-	int a[N], val[N];
+	vector<bool> v(N);
+	fill(v.end() - R, v.end(), true); 
+	int a[N], val[N], b[R];
 	for(int i = 0; i < N; i++)
 		a[i] = i;
 
+	
 	do{
-		for(int i = 0; i < N; i++)
-			val[i] = i;
-		for(int i = 0; i < N-1; i++){
-			if(a[i] > a[i+1]){
-				val[a[i+1]] = val[a[i]];
-			} else {
-				val[a[i]] = val[a[i+1]];
+
+		int counter = 0;
+		for(int i = 0; i < N; i++){
+			if(v[i]){
+				b[counter] = a[i];
+				cout << b[counter] << "\t";
+				counter++;
 			}
 		}
-		for(int i = 0; i < N; i++)
-			cout << a[i];
-		cout << "\t";
-		for(int i = 0; i < N; i++)
-			cout << val[i];
 		cout << endl;
-		uint64_t res = 0;
-		for(int i = 0; i < N; i++){
-			res *= 10;
-			res += val[i];
-		}
-		string temp = to_string(res);
-		m[temp]++;
-	} while (next_permutation(a, a + N));
+		do{
+			for(int i = 0; i < N; i++)
+				val[i] = i;
+			for(int i = 0; i < R - 1; i++){
+				if(b[i] > b[i+1]){
+					val[b[i+1]] = val[b[i]];
+				} else {
+					val[b[i]] = val[b[i+1]];
+				}
+			}
+			for(int i = 0; i < R; i++)
+				cout << b[i];
+			cout << "\t";
+			for(int i = 0; i < R; i++)
+				cout << val[b[i]];
+			cout << endl;
+			string res = "000";
+			for(int i = 0; i < R; i++){
+				res[i] = val[b[i]] + '0';
+			}
+			m[res]++;
+		} while (next_permutation(b, b + R));
+	} while (next_permutation(v.begin(), v.end()));
+
 	int counter = 0;
 	for(it = m.begin(); it != m.end(); it++)
 		if(it->second > 0){

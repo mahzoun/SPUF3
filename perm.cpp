abcb97850ae54464d877599f55db21b459a2d631
mahzoun <mohammad.mahzoun@gmail.com>
2021-05-11 12:03:49 +0200

    extend attack to two columns

--- perm.cpp
+++ perm.cpp
@@ -11,61 +11,91 @@ const int N = 4, R = 3;
 using namespace std;
 
 void perm_exp(){
-	map<string, uint64_t> m;
-	map<string, uint64_t>::iterator it;
-	vector<bool> v(N);
-	fill(v.end() - R, v.end(), true); 
-	int a[N], val[N], b[R];
-	for(int i = 0; i < N; i++)
-		a[i] = i;
+	vector<bool> v[2];
+	v[0].resize(N, 0);
+	v[1].resize(N, 0);
+	fill(v[0].end() - R, v[0].end(), true); 
+	fill(v[1].end() - R, v[1].end(), true); 
+	int a[2][N], val[2][N], b[2][R];
+	for(int i = 0; i < N; i++) {
+		a[0][i] = i;
+		a[1][i] = i;
+	}
 	
 	int total_ch = 0;
 	
 	do{
 
+		map<string, uint64_t> m;
+		map<string, uint64_t>::iterator it;
 		int counter = 0;
 		for(int i = 0; i < N; i++){
-			if(v[i]){
-				b[counter] = a[i];
-				cout << b[counter] << "\t";
+			if(v[0][i]){
+				b[1][counter] = a[1][i];
+				//cout << b[0][counter] << "\t";
 				counter++;
 			}
 		}
-		cout << endl;
 		do{
-			total_ch++;
-			for(int i = 0; i < N; i++)
-				val[i] = i;
-			for(int i = 0; i < R - 1; i++){
-				if(b[i] > b[i+1]){
-					val[b[i+1]] = val[b[i]];
-				} else {
-					val[b[i]] = val[b[i+1]];
+			int counter = 0;
+			for(int i = 0; i < N; i++){
+				if(v[0][i]){
+					b[0][counter] = a[0][i];
+					//cout << b[1][counter] << "\t";
+					counter++;
 				}
 			}
-			for(int i = 0; i < R; i++)
-				cout << b[i] << " ";
-			cout << "\t";
-			for(int i = 0; i < N; i++)
-				cout << val[i] << " ";
-			cout << endl;
-			string res = "";
-			for(int i = 0; i < N; i++)
-				res += "0";
-			for(int i = 0; i < N; i++){
-				res[i] = val[i] + '0';
-			}
-			m[res]++;
-		} while (next_permutation(b, b + R));
-	} while (next_permutation(v.begin(), v.end()));
+			do{
+				for(int i = 0; i < N; i++){
+					val[0][i] = i;
+					val[1][i] = i;
+				}
 
-	int counter = 0;
-	for(it = m.begin(); it != m.end(); it++)
-		if(it->second > 0){
-			cout << it->first << "\t" << it->second <<endl;
-			counter++;
-		}
-	cout << total_ch << "\t" << counter << endl;
+				for(int i = 0; i < R - 1; i++){
+					if(b[0][i] > b[0][i+1]){
+						val[0][b[0][i+1]] = val[0][b[0][i]];
+					} else {
+						val[0][b[0][i]] = val[0][b[0][i+1]];
+					}
+
+					if(b[1][i] > b[1][i+1]){
+						val[1][b[1][i+1]] = val[1][b[1][i]];
+					} else {
+						val[1][b[1][i]] = val[1][b[1][i+1]];
+					}
+
+				}
+				for(int i = 0; i < R; i++)
+					cout << b[0][i] << " ";
+				cout << "\t";
+				for(int i = 0; i < R; i++)
+					cout << b[1][i] << " ";
+				cout << "\t";
+				string res = "";
+				for(int i = 0; i < 2*N; i++)
+					res += "0";
+				for(int i = 0; i < N; i++){
+					res[i] = val[0][i] + '0';
+				}
+				for(int i = 0; i < N; i++){
+					res[N + i] = val[1][i] + '0';
+				}
+				cout << res << endl;
+				m[res]++;
+				total_ch++;
+				//second columns
+			} while (next_permutation(b[0], b[0] + R));
+		} while (next_permutation(v[0].begin(), v[0].end()));
+		counter = 0;
+		for(it = m.begin(); it != m.end(); it++)
+			if(it->second > 0){
+				cout << it->first << "\t" << it->second <<endl;
+				counter++;
+			}
+		cout << total_ch << "\t" << counter << endl;
+		cout << endl;
+	} while (next_permutation(a[1], a[1] + N));
+
 }
 
 void seq_exp(){
